3ca35f8f7a574798e7cb6fa8d9ca9b1302ea0350
Adampratnicki <adampratnickistudio@gmail.com>
2022-03-30 23:22:14 +0200

    Upload All files

---
+++ Scripts/firebase.init.js
@@ -0,0 +1,13 @@
+﻿// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAW3vEPslIUNdHxkzh8QI1zgljX3qV1Iss",
+    authDomain: "chat-app-48997.firebaseapp.com",
+    databaseURL: "https://chat-app-48997.firebaseio.com",
+    projectId: "chat-app-48997",
+    storageBucket: "chat-app-48997.appspot.com",
+    messagingSenderId: "301846460247",
+    appId: "1:301846460247:web:8f5dc65199d1387feeb012"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
